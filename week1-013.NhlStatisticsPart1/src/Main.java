e2d00b88f43bc22d4eea56f1473ec52722e2b77b
Arianna Lang Wang <32724597+ariannalangwang@users.noreply.github.com>
2019-08-12 05:37:06 -0700

    Add files via upload

---
+++ week1-013.NhlStatisticsPart1/src/Main.java
@@ -0,0 +1,35 @@
+
+import nhlstats.NHLStatistics;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Top ten players based on goals");
+        NHLStatistics.sortByGoals();
+        NHLStatistics.top(10);
+        
+        System.out.println("");
+        System.out.println("Top 25 players based on penalty amounts");
+        NHLStatistics.sortByPenalties();
+        NHLStatistics.top(25);
+        
+        System.out.println("");
+        System.out.println("Statistics for Sidney Crosby");
+        NHLStatistics.searchByPlayer("Sidney Crosby");
+        
+        System.out.println("");
+        System.out.println("Statistics for Philadelphia Flyers");
+        NHLStatistics.teamStatistics("PHI");
+        
+        System.out.println("");
+        System.out.println("Players in Anaheim Ducks ordered by points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.teamStatistics("ANA");
+        
+        
+        
+    }
+}
+
+
+ 
